4b6a162c261d5491617c124c480eacc1af1ba702
SkyView <skyviewgithubgmail.com>
2026-06-06 16:18:04 +0200

    fix: avoid stale hosted bundle

--- tests/loader.test.js
+++ tests/loader.test.js
@@ -12,14 +12,18 @@ test('Tampermonkey loader targets both Blobgame hosts and fetches the GitHub bun
 
   assert.match(loader, /\/\/ @match\s+\*:\/\/blobgame\.io\/\*/);
   assert.match(loader, /\/\/ @match\s+\*:\/\/custom\.client\.blobgame\.io\/\*/);
+  assert.match(loader, /\/\/ @version\s+0\.1\.1/);
   assert.match(loader, /\/\/ @run-at\s+document-start/);
   assert.match(loader, /\/\/ @grant\s+GM_xmlhttpRequest/);
   assert.match(loader, /\/\/ @connect\s+cdn\.jsdelivr\.net/);
   assert.match(loader, /\/\/ @connect\s+raw\.githubusercontent\.com/);
   assert.match(loader, /\/\/ @downloadURL\s+https:\/\/raw\.githubusercontent\.com\/SkyViewBlobio\/Blobgame\.io-Web-Script\/main\/loader\/blobio-loader\.user\.js/);
   assert.match(loader, /\/\/ @updateURL\s+https:\/\/raw\.githubusercontent\.com\/SkyViewBlobio\/Blobgame\.io-Web-Script\/main\/loader\/blobio-loader\.user\.js/);
-  assert.match(loader, /https:\/\/cdn\.jsdelivr\.net\/gh\/SkyViewBlobio\/Blobgame\.io-Web-Script@main\/dist\/blobio-extension\.bundle\.js/);
-  assert.match(loader, /https:\/\/raw\.githubusercontent\.com\/SkyViewBlobio\/Blobgame\.io-Web-Script\/main\/dist\/blobio-extension\.bundle\.js/);
+  const rawBundleUrlIndex = loader.indexOf('https://raw.githubusercontent.com/SkyViewBlobio/Blobgame.io-Web-Script/main/dist/blobio-extension.bundle.js?v=0.1.1');
+  const cdnBundleUrlIndex = loader.indexOf('https://cdn.jsdelivr.net/gh/SkyViewBlobio/Blobgame.io-Web-Script@main/dist/blobio-extension.bundle.js?v=0.1.1');
+  assert.notEqual(rawBundleUrlIndex, -1);
+  assert.notEqual(cdnBundleUrlIndex, -1);
+  assert.equal(rawBundleUrlIndex < cdnBundleUrlIndex, true);
   assert.match(loader, /GM_xmlhttpRequest/);
   assert.match(loader, /\[Blobio\]/);
 });
